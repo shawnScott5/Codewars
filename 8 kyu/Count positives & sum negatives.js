a5803a015a4180c116083b249007ec523c84cfdd
Shawn Scott <shawnscott5694@gmail.com>
2022-01-25 22:25:40 -0500

    Codewars Solutions

---
+++ 8 kyu/Count positives & sum negatives.js
@@ -0,0 +1,29 @@
+/* Instructions: 
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers 
+and the second element is sum of negative numbers. 0 is neither positive 
+nor negative.
+
+If the input array is empty or null, return an empty array.
+*/
+
+// Solution:
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0)
+      return [];
+    
+    var positive = 0;
+    var negative = 0;
+    
+    for (var i=0; i<input.length; i++){
+      
+      if (input[i] > 0)
+        positive++;
+      
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
+}
---
+++ 8 kyu/Even or Odd.js
@@ -0,0 +1,9 @@
+/* Instructions: 
+Create a function that takes an integer as an argument and returns 
+"Even" for even numbers or "Odd" for odd numbers. 
+*/
+
+// Solution:
+  function even_or_odd(number) {
+    return number % 2 === 0 ? "Even" : "Odd";
+ }
