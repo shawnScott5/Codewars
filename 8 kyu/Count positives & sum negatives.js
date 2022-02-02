9724dae3446e9cd6db6dacdaf24bc1510c6e1821
Shawn Scott <shawnscott5694@gmail.com>
2022-02-01 22:20:42 -0500

    Codewars solutions

--- 8 kyu/Count positives & sum negatives.js
+++ 8 kyu/Count positives & sum negatives.js
@@ -13,10 +13,10 @@ function countPositivesSumNegatives(input) {
     if (input == null || input.length == 0)
       return [];
     
-    var positive = 0;
-    var negative = 0;
+    let positive = 0;
+    let negative = 0;
     
-    for (var i=0; i<input.length; i++){
+    for (let i=0; i<input.length; i++){
       
       if (input[i] > 0)
         positive++;
