a334044ffb16c5007fc165205e344fd2e02c941c
Shawn Scott <sesjr94@gmail.com>
2022-02-20 23:41:29 -0500

    Codewars solutions

---
+++ 7 kyu/Area or Perimeter.js
@@ -0,0 +1,10 @@
+/* Instructions: 
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+area_or_perimeter(6, 10) --> 32
+area_or_perimeter(3, 3) --> 9
+*/
+
+// Solution:
+const areaOrPerimeter = (l , w) => l === w ? l * w : 2 * (l + w);
